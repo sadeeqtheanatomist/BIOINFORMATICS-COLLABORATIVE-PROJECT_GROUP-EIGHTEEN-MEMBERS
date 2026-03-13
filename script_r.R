468316f8b90e0f80cb54e786d95bdf8288ec9c39
sadeeqtheanatomist <abubakaraigrm@gmail.com>
2026-03-13 22:51:00 +0100

    Add author information to script_r.R

---
+++ script_r.R
@@ -0,0 +1,6 @@
+# ANA4315 Bioinformatics CA1 - R Script
+# Author: Abubakar Abdulkadir
+cat("Abubakar Abdulkadir\n")
+cat("abubakaraigrm@gmail.com\n")
+cat("@abubakarsirikinchiroma\n")
+cat("proteogenomics\n")
