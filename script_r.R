f105bcb643ab033116f93c0ae9450bb4f98608f1
sadeeqtheanatomist <abubakaraigrm@gmail.com>
2026-03-14 08:30:48 +0100

    Update script_r.R

--- script_r.R
+++ script_r.R
@@ -1,5 +1,6 @@
 # ANA4315 Bioinformatics CA1 - R Script
 # Author: Abubakar Abdulkadir
+#GitHub username: sadeeqtheanatomist
 cat("Abubakar Abdulkadir\n")
 cat("abubakaraigrm@gmail.com\n")
 cat("@abubakarsirikinchiroma\n")
